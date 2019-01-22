b4b2746957e7ad444208dda8b0f0b66968163943
Fayvor George <phavorsparks@gmail.com>
2019-01-22 09:11:36 +0100

    setup the blog schema

---
+++ model/schema.js
@@ -0,0 +1,20 @@
+const moment = require('moment')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+  title: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+  body: String,
+  createdAt: { type: Date, default: moment() }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+Blog.create({
+  title: 'Test Blog',
+  imgUrl: 'https://photosforclass.com/download/flickr-1342367857',
+  body: "this is our test blog"
+})
+
+module.exports = Blog
