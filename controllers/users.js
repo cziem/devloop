24c9c4dd4ed38156b687883f63ac9c525aac89c0
Fayvor George <phavorsparks@gmail.com>
2019-02-09 22:55:37 +0100

    handle post route logic

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,12 @@ const User = require('../model/user')
 module.exports = {
   // Get Admin Login Page
   AdminLogin: (req, res) => {
-    res.render('admin')
+    res.render('users/admin')
+  },
+
+  // Handle Admin Registration
+  adminReg: (req, res) => {
+    res.send('Hanling request...')
+    console.log(req.body)
   }
 }
