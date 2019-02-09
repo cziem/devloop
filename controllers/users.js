0adfcfe784badaa7bdd0c540f302c51f09113ba7
Fayvor George <phavorsparks@gmail.com>
2019-02-09 23:58:17 +0100

    setup logic to add admin to DB

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,12 @@
-// import { model } from "mongoose";
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+
 const User = require('../model/user')
+
+// Configure Passport
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 module.exports = {
   // Get Admin Login Page
@@ -8,8 +15,38 @@ module.exports = {
   },
 
   // Handle Admin Registration
-  adminReg: (req, res) => {
-    res.send('Hanling request...')
-    console.log(req.body)
+  adminReg: async (req, res) => {
+    const newAdmin = new User({
+      username: req.body.username,
+      email: req.body.email
+    })
+
+    const { password } = req.body
+
+    if (req.body.adminCode === process.env.ADMIN_CODE) {
+      newAdmin.isAdmin = true
+
+      try {
+        let admin = await User.register(newAdmin, password)
+        passport.authenticate('local')(req, res, () => {
+          res.json({
+            admin,
+            status: 200
+          })
+          res.render('user/dashboard', admin)
+        })
+      } catch (error) {
+        res.send(`Sorry new Admin ${req.body.username}, could not be registered...`)
+        console.log(error)
+      }
+    } else {
+      console.log('You dont have admin code')
+      const err = {
+        status: 401,
+        message: `You have no authorized passcode for this operation. Contact devloops admin <a href="mailto:help@devloops.com">here</a>`
+      }
+      res.render('users/admin', { err })
+    }
+
   }
 }
