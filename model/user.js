1b9e5f3aa3354096e828874bb1c2955f36190a81
Fayvor George <phavorsparks@gmail.com>
2019-02-09 20:55:48 +0100

    Added the user model

---
+++ model/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require('passport-local-mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  isAdim: { type: Boolean, default: false },
+  avatar: String,
+  avatarId: String
+});
+
+userSchema.plugin(passportLocalMongoose)
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
