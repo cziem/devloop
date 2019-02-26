f86c8dafdafe62139b3b4d7c947625ced439669f
Fayvor George <phavorsparks@gmail.com>
2019-02-26 12:50:34 +0100

    Add likes functionality to the show page

--- controllers/app.js
+++ controllers/app.js
@@ -1,106 +1,116 @@
-const Blog = require('../model/schema')
+const Blog = require("../model/schema");
 
 module.exports = {
   // Home page
   redirect: (req, res) => {
     // For now we will redirect to the blog page
-    res.redirect('/blogs')
+    res.redirect("/blogs");
   },
 
   // INDEX ROUTE
   getBlogs: (req, res) => {
-    Blog
-      .find()
+    Blog.find()
       .sort({ createdAt: -1 })
       .then(blogs => {
-        res.render('index', { blogs })
-      })
+        res.render("index", { blogs });
+      });
   },
 
   // NEW POST FORM
   newBlog: (req, res) => {
     // Render a form for new Blog post creation
-    res.render('new')
+    res.render("new");
   },
 
   // CREATE BLOG
   createBlog: (req, res) => {
     // Handle form data coming from new blog post page
     // sanitize
-    req.body.blog.body = req.sanitize(req.body.blog.body)
+    req.body.blog.body = req.sanitize(req.body.blog.body);
 
     const author = {
       id: req.user._id,
       username: req.user.username
-    }
+    };
 
-    const post = req.body.blog
-    post.author = author
-    
+    const post = req.body.blog;
+    post.author = author;
+
     Blog.create(post)
-      .then((blog) => {
-        res.redirect('/blogs')
+      .then(blog => {
+        res.redirect("/blogs");
       })
       .catch(err => {
-        req.flash('error', 'Could not create your post!')
-        res.render('new')
-      })
+        req.flash("error", "Could not create your post!");
+        res.render("new");
+      });
   },
 
   // SHOW BLOG
   showBlog: (req, res) => {
     // Show details about a blog using it's id
-    const blogId = req.params.id
+    const blogId = req.params.id;
     Blog.findById(blogId)
       .then(blog => {
-        res.render('show', { blog })
+        res.render("show", { blog });
       })
       .catch(() => {
         // render Error page later
-        res.redirect('/blogs')
-      })
+        res.redirect("/blogs");
+      });
   },
 
   // EDIT BLOG
   editBlog: (req, res) => {
     // Render form for editing a Blog post
-    const blogId = req.params.id
+    const blogId = req.params.id;
     Blog.findById(blogId)
       .then(blog => {
-        res.render('edit', { blog })
+        res.render("edit", { blog });
       })
       .catch(() => {
-        res.redirect('/blogs')
-      })
+        res.redirect("/blogs");
+      });
   },
 
   // UPDATE BLOG
   updateBlog: (req, res) => {
     // update blog details by blog's id
     // sanitize
-    req.body.blog.body = req.sanitize(req.body.blog.body)
+    req.body.blog.body = req.sanitize(req.body.blog.body);
 
-    const blogId = req.params.id
-    const updatedBlog = req.body.blog
-    Blog.findByIdAndUpdate(blogId,  updatedBlog )
+    const blogId = req.params.id;
+    const updatedBlog = req.body.blog;
+    Blog.findByIdAndUpdate(blogId, updatedBlog)
       .then(blog => {
-        res.redirect(`/blogs/${blogId}`)
+        res.redirect(`/blogs/${blogId}`);
       })
       .catch(() => {
-        res.redirect('/blogs')
-      })
+        res.redirect("/blogs");
+      });
   },
 
   // DELETE BLOG
   deleteBlog: (req, res) => {
     // delete a blog post by it's id
-    const blogId = req.params.id
+    const blogId = req.params.id;
     Blog.findByIdAndDelete(blogId)
-      .then((data) => {
-        res.json(data)
+      .then(data => {
+        res.json(data);
       })
-      .catch((err) => {
-        res.redirect('/blogs')
+      .catch(err => {
+        res.redirect("/blogs");
+      });
+  },
+
+  // LIKE A POST
+  likePost: (req, res) => {
+    Blog.findById(req.params.id)
+      .then(async blog => {
+        blog.likes++;
+        let likedBlog = await blog.save();
+        res.json(likedBlog);
       })
+      .catch(err => console.log(err));
   }
-}
+};
--- public/js/flash.js
+++ public/js/flash.js
@@ -1,33 +1,61 @@
-$('document').ready(() => {
-  $('.close').click(() => {
-    $('.flash').hide(100)
-  })
+$("document").ready(() => {
+  $(".close").click(() => {
+    $(".flash").hide(100);
+  });
 
   setTimeout(() => {
-    $('.flash').hide(100)
+    $(".flash").hide(100);
   }, 5000);
 
-  $('#deletePost').click((event) => {
-    event.preventDefault()
-    $('.ui.modal')
+  $("#deletePost").click(event => {
+    event.preventDefault();
+    $(".ui.modal")
       .modal({
         closable: false,
         onDeny: () => {
-          return
+          return;
         },
-        onApprove: (data) => {
-          let blogId = window.location.pathname.split('/')[2]
-          let url = '/blogs/' + blogId
+        onApprove: data => {
+          let blogId = window.location.pathname.split("/")[2];
+          let url = "/blogs/" + blogId;
           $.ajax({
             url: url,
-            type: 'DELETE',
+            type: "DELETE",
             timeout: 5000,
             success: function(data) {
-              location.replace('/blogs')
+              location.replace("/blogs");
             }
-          })
+          });
         }
       })
-      .modal('show')
-  })
-})
+      .modal("show");
+  });
+
+  $("#likes").click(event => {
+    event.preventDefault();
+    let blogId = window.location.pathname.split("/")[2];
+    let url = "/blogs/" + blogId + "/like";
+
+    $.ajax({
+      url,
+      type: "POST",
+      success: function(data) {
+        $("#like-count").text(data.likes);
+      }
+    });
+  });
+
+  $(".ui.circular.red").click(event => {
+    event.preventDefault();
+    let blogId = window.location.pathname.split("/")[2];
+    let url = "/blogs/" + blogId + "/like";
+
+    $.ajax({
+      url,
+      type: "POST",
+      success: function(data) {
+        $("#like-count").text(data.likes);
+      }
+    });
+  });
+});
