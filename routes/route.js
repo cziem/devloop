90a05b2bf3ad228c66f41903eac780290e46ddd0
Fayvor George <phavorsparks@gmail.com>
2019-02-26 12:30:57 +0100

    add likes route

--- routes/route.js
+++ routes/route.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { isLoggedIn } = require('../middlewares/auth')
+const { isLoggedIn } = require("../middlewares/auth");
 
-const controller = require('../controllers/app')
+const controller = require("../controllers/app");
 
 // Get all blogs
-router.get('/', controller.redirect)
-router.get('/blogs', controller.getBlogs)
+router.get("/", controller.redirect);
+router.get("/blogs", controller.getBlogs);
 
 // Create new blog post
-router.get('/blogs/new', isLoggedIn, controller.newBlog)
-router.post('/blogs', isLoggedIn, controller.createBlog)
+router.get("/blogs/new", isLoggedIn, controller.newBlog);
+router.post("/blogs", isLoggedIn, controller.createBlog);
 
 // Display a single blog post
-router.get('/blogs/:id', controller.showBlog)
+router.get("/blogs/:id", controller.showBlog);
 
 // Edit and update a blog post
-router.get('/blogs/:id/edit', isLoggedIn, controller.editBlog)
-router.put('/blogs/:id', isLoggedIn, controller.updateBlog)
+router.get("/blogs/:id/edit", isLoggedIn, controller.editBlog);
+router.put("/blogs/:id", isLoggedIn, controller.updateBlog);
 
 // Delete a blog post
-router.delete('/blogs/:id', isLoggedIn, controller.deleteBlog)
+router.delete("/blogs/:id", isLoggedIn, controller.deleteBlog);
 
-module.exports = router
+// Like a blog post
+router.post("/blogs/:id/like", controller.likePost);
+
+module.exports = router;
